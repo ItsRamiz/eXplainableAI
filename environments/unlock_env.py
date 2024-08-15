9c022218bfd72ce192f3e6d9c1364e11fc9d55ae
Ramiz Srour <ramiz.srour1@gmail.com>
2024-08-15 14:36:55 +0300

    Ramiz Laptop - Unlock Env

--- environments/unlock_env.py
+++ environments/unlock_env.py
@@ -12,29 +12,59 @@ from flask import redirect, url_for
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from videos.video_utils import convert_to_mp4, extend_video
 
+
+def is_door_open(env):
+    grid = env.unwrapped.grid.encode()
+    width, height, _ = grid.shape
+    for i in range(width):
+        for j in range(height):
+            cell = grid[i, j]
+            obj_idx, color_idx, state = cell
+            if obj_idx == 4:
+                return state == 0
+    return False
+
+
+def get_key_color(env):
+    for x in range(env.grid.width):
+        for y in range(env.grid.height):
+            obj = env.grid.get(x, y)
+            if isinstance(obj, Door):
+                return obj.color
+
+
 def submit_unlock_env(request):
     if request.method == 'POST':
         try:
-            # Sanitize input data
-            min_duration = int(request.form['minDuration'].replace(',', ''))
-            max_steps = int(request.form['maxSteps'].replace(',', ''))
-            min_steps = int(request.form['minSteps'].replace(',', ''))
+            # Sanitize input data with default value -999 for empty fields
+            min_duration = int(request.form['minDuration'].replace(',', '')) if request.form['minDuration'] else 0
+            max_steps = int(request.form['maxSteps'].replace(',', '')) if request.form['maxSteps'] else 1000000
+            min_steps = int(request.form['minSteps'].replace(',', '')) if request.form['minSteps'] else 0
+            door_position = int(request.form['doorPosition'].replace(',', '')) if request.form['doorPosition'] else -999
+            max_steps_until_key = int(request.form['stepsUntilKey'].replace(',', '')) if request.form[
+                'stepsUntilKey'] else 1000000
+
+            # Check for checkbox and select fields
             is_winner = 1 if 'isWinner' in request.form else 0
             hit_a_wall = 1 if 'hitAWall' in request.form else 0
-            door_position = int(request.form['doorPosition'].replace(',', ''))
-            key_color = request.form['keyColor']
-            steps_until_key = int(request.form['stepsUntilKey'].replace(',', ''))
-            agent_type = request.form['agentType']
+            key_color = request.form['keyColor'] if 'keyColor' in request.form and request.form[
+                'keyColor'] else 'unknown'
+            agent_type = request.form['agentType'] if 'agentType' in request.form and request.form[
+                'agentType'] else 'unknown'
+
             if agent_type == 'ppo':
                 agent_model_path = Path(r'storage\model.zip')
             elif agent_type == 'rl_starter':
                 agent_model_path = Path(r'storage\Unlockenv')
+            else:
+                agent_model_path = Path('')
 
             if not agent_model_path.exists():
                 return f"Model file not found: {agent_model_path}"
 
             # Collect user inputs
-            user_inputs = [min_duration, max_steps, min_steps, is_winner, hit_a_wall, door_position, key_color, steps_until_key]
+            user_inputs = [min_duration, max_steps, min_steps, is_winner, hit_a_wall, door_position, key_color,
+                           max_steps_until_key]
 
             # Process the videos based on user inputs and agent type
             process_videos_unlock(user_inputs, agent_type, agent_model_path)
@@ -47,7 +77,34 @@ def submit_unlock_env(request):
     else:
         return "Request method is not POST"
 
-def process_videos_unlock(user_inputs, agent_type, agent_model_path):
+
+def query_unlock(user_inputs):
+    vector = []
+    vector.append(1 if user_inputs[0] > 0 and user_inputs[0] <= 5 else 0)
+    vector.append(1 if user_inputs[1] > 0 else 0)
+    vector.append(1 if user_inputs[2] > 0 else 0)
+    vector.append(1 if user_inputs[3] == 1 else 0)
+    vector.append(1 if user_inputs[4] == 1 else 0)
+    vector.append(1 if user_inputs[5] > 0 and user_inputs[5] <= 5 else 0)
+    vector.append(1 if user_inputs[6] in ["red", "green", "blue", "purple", "yellow", "grey"] else 0)
+    return vector
+
+
+def checkGameState(numberSteps, isWinner, isHitWall, KeyColor, MaxStepUntilKey, user_inputs):
+    # [min_duration, max_steps, min_steps, is_winner, hit_a_wall, door_position, key_color, max_steps_until_key]
+    #      0           1          2           3         4             5          6          7
+    #   IGNORED                                                     IGNORED
+
+    if numberSteps <= user_inputs[1] and isWinner == user_inputs[3] and isHitWall == user_inputs[4] and KeyColor == \
+            user_inputs[6] and MaxStepUntilKey <= user_inputs[7]:
+        return True
+    else:
+        return False
+
+
+def process_videos_unlock(user_inputs, agent_type,
+                          agent_model_path):  # [min_duration, max_steps, min_steps, is_winner, hit_a_wall, door_position, key_color, max_steps_until_key]
+    # USER INPUT MAPPING LEGEND
     video_dir = Path("static/videos")
     video_dir.mkdir(parents=True, exist_ok=True)
     for video_file in video_dir.glob("*.mp4"):
@@ -67,100 +124,47 @@ def process_videos_unlock(user_inputs, agent_type, agent_model_path):
         values = [0, 1, 2, 3, 4, 5, 6, 10]
         probabilities = [0.1, 0.1, 0.1, 0, 0.1, 0, 0, 0.6]
         videos_to_extract = []
-        number_of_steps = 0
 
         isPickedKey = False
 
         for episode_id in range(10):
-            first = None
-            end = None
-            v = [0, 0, 0, 0, 0]
+
+            GameStateTrack = [0, 0, 0, 0, 0, 0, 0, 0]
             curr_number_of_steps = 0
             observation, info = env.reset()
             video_saved = False
             isPickedKey = False
+            isWinner = False
+            isHitWall = False
+            KeyColor = get_key_color(env)
+            StepUntilKey = 0
+
             for step in range(10000):
-                number_of_steps += 1
+
                 curr_number_of_steps += 1
 
                 action, _states = model.predict(observation, deterministic=False)
 
-                if(action == 3 and (get_key_color(env) == "red" or get_key_color(env) == "blue")):
+                if (action == 3):
+                    StepUntilKey = curr_number_of_steps
                     isPickedKey = True
-
-                if(isPickedKey == True):
-                    values.pop()
-                    values.append(action)
-                    action = np.random.choice(values, size=1, p=probabilities)
 
                 observation, reward, terminated, truncated, info = env.step(action)
 
-                user_check = query_unlock(user_inputs)
-
-                if user_check[1] == 1:
-                    if curr_number_of_steps <= user_inputs[1]:
-                        v[0] = 1
-                        if first is None:
-                            first = curr_number_of_steps
-                    else:
-                        v[0] = 0
-                        first = curr_number_of_steps
-                        v = [0, 0, 0, 0, 0]
-
-                if user_check[2] == 1:
-                    if curr_number_of_steps >= user_inputs[2]:
-                        v[1] = 1
-                        if first is None:
-                            first = curr_number_of_steps
-                    else:
-                        v[1] = 0
-                        first = curr_number_of_steps
-                        v = [0, 0, 0, 0, 0]
-
-                if user_check[3] == 1:
-                    if is_door_open(env):
-                        v[2] = 1
-                        if first is None:
-                            first = curr_number_of_steps
-                    else:
-                        v[2] = 0
-                else:
-                    if not is_door_open(env):
-                        v[2] = 1
-                        if first is None:
-                            first = curr_number_of_steps
-                    else:
-                        v[2] = 0
-
-                if user_check[4] == 1:
-                    if action == 3:
-                        action = 0
-                        v[3] = 1
-                        if first is None:
-                            first = curr_number_of_steps
-                    else:
-                        v[3] = 0
-                else:
-                    if action != 3:
-                        v[3] = 1
-                        if first is None:
-                            first = curr_number_of_steps
-                    else:
-                        v[3] = 0
-
-                v[4] = 1
+                if (reward > 0):
+                    isWinner = 1
 
                 if terminated or truncated:
-                    isPickedKey = False
-                    if all(v):
-                        if (curr_number_of_steps - first) >= (user_inputs[0] * frame_rate):
-                            end = curr_number_of_steps
-                            videos_to_extract.append((episode_id, first, end))
+
+                    if checkGameState(curr_number_of_steps, isWinner, isHitWall, KeyColor, StepUntilKey):
+                        if curr_number_of_steps >= (user_inputs[0] * frame_rate):
+                            videos_to_extract.append((episode_id, 0, curr_number_of_steps))
                             video_saved = True
                     break
 
             if not video_saved:
-                env = gym.wrappers.RecordVideo(env, video_folder=str(video_dir), episode_trigger=lambda episode_id: False)
+                env = gym.wrappers.RecordVideo(env, video_folder=str(video_dir),
+                                               episode_trigger=lambda episode_id: False)
 
         env.close()
 
@@ -184,7 +188,8 @@ def process_videos_unlock(user_inputs, agent_type, agent_model_path):
                             argmax=False, use_memory=False, use_text=False)
         print("Agent loaded\n")
 
-        env = gym.wrappers.RecordVideo(env, video_folder=str(video_dir), episode_trigger=lambda episode_id: True)  # Add RecordVideo wrapper
+        env = gym.wrappers.RecordVideo(env, video_folder=str(video_dir),
+                                       episode_trigger=lambda episode_id: True)  # Add RecordVideo wrapper
 
         frame_rate = 15
         values = [0, 1, 2, 3, 4, 5, 6, 10]
@@ -211,10 +216,10 @@ def process_videos_unlock(user_inputs, agent_type, agent_model_path):
                 obs, reward, terminated, truncated, _ = env.step(action)
                 done = terminated or truncated
 
-                if(action == 3 and (get_key_color(env) == "red" or get_key_color(env) == "blue")):
+                if (action == 3 and (get_key_color(env) == "red" or get_key_color(env) == "blue")):
                     isPickedKey = True
 
-                if(isPickedKey == True):
+                if (isPickedKey == True):
                     values.pop()
                     values.append(action)
                     action = np.random.choice(values, size=1, p=probabilities)
@@ -284,9 +289,14 @@ def process_videos_unlock(user_inputs, agent_type, agent_model_path):
                     break
 
             if not video_saved:
-                env = gym.wrappers.RecordVideo(env, video_folder=str(video_dir), episode_trigger=lambda episode_id: False)
+                env = gym.wrappers.RecordVideo(env, video_folder=str(video_dir),
+                                               episode_trigger=lambda episode_id: False)
 
         env.close()
+
+        #######################
+        #### Saving Videos ####
+        #######################
 
         for video_info in videos_to_extract:
             episode_id, first, end = video_info
@@ -305,32 +315,3 @@ def process_videos_unlock(user_inputs, agent_type, agent_model_path):
         episode_id = video_info[0]
         output_clip_path = clips_dir / f"output_clip_episode_{episode_id}.mp4"
         extend_video(output_clip_path, extension_factor=2)
-
-def query_unlock(user_inputs):
-    vector = []
-    vector.append(1 if user_inputs[0] > 0 and user_inputs[0] <= 5 else 0)
-    vector.append(1 if user_inputs[1] > 0 else 0)
-    vector.append(1 if user_inputs[2] > 0 else 0)
-    vector.append(1 if user_inputs[3] == 1 else 0)
-    vector.append(1 if user_inputs[4] == 1 else 0)
-    vector.append(1 if user_inputs[5] > 0 and user_inputs[5] <= 5 else 0)
-    vector.append(1 if user_inputs[6] in ["red", "green", "blue", "purple", "yellow", "grey"] else 0)
-    return vector
-
-def is_door_open(env):
-    grid = env.unwrapped.grid.encode()
-    width, height, _ = grid.shape
-    for i in range(width):
-        for j in range(height):
-            cell = grid[i, j]
-            obj_idx, color_idx, state = cell
-            if obj_idx == 4:
-                return state == 0
-    return False
-
-def get_key_color(env):
-    for x in range(env.grid.width):
-        for y in range(env.grid.height):
-            obj = env.grid.get(x, y)
-            if isinstance(obj, Door):
-                return obj.color
